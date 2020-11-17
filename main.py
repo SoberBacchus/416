35e1632d8abc9aff1e776bb1a2eb74447564edfd
Christopher Bi <cxb1@rice.edu>
2020-11-17 13:52:05 -0500

    test

--- main.py
+++ main.py
@@ -20,3 +20,15 @@ def home_base():
     """
     read io of fasta and fastq
     """
+
+from Bio import SeqIO
+
+short_sequences = []  # Setup an empty list
+for record in SeqIO.parse("cor6_6.gb", "genbank"):
+    if len(record.seq) < 300:
+        # Add this record to our list
+        short_sequences.append(record)
+
+print("Found %i short sequences" % len(short_sequences))
+
+SeqIO.write(short_sequences, "short_seqs.fasta", "fasta")
