29fba1791c57833a35bfe79cb80962ae9b65ede3
Christopher Bi <60297010+SoberBacchus@users.noreply.github.com>
2020-11-30 19:24:53 -0500

    Update maestro.py

--- Working/maestro.py
+++ Working/maestro.py
@@ -3,12 +3,11 @@ n=number
 path1 = "ref.fasta"
 pathX = "sample_2.fastq"
 pathC = "mapping_ground_truth.txt"
-import main
 
 def run():
     print("lets rumblee")
-    import time
     t = time.time()
+    import main
     main.runengine(number, path1, pathX)
     time = time.time()-t
 
